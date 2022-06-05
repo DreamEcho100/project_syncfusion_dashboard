9eb4eaa22d31081a631cb5550a6334cef5eebc18
DreamEcho100 <maze6572198@gmail.com>
2022-06-05 19:06:18 +0200

    Building a custom 'TooltipComponent' component

---
+++ components/index.jsx
@@ -0,0 +1,33 @@
+// export { default as Button } from './dependent/Button';
+// export { default as ThemeSettings } from './dependent/ThemeSettings';
+// export { default as Sidebar } from './dependent/Sidebar';
+// export { default as Navbar } from './dependent/Navbar';
+// export { default as Footer } from './dependent/Footer';
+// export { default as Cart } from './dependent/Cart';
+// export { default as Chat } from './dependent/Chat';
+// export { default as Notification } from './dependent/Notification';
+// export { default as UserProfile } from './dependent/UserProfile';
+export { default as SparkLine } from './dependent/Charts/SparkLine';
+// export { default as LineChart } from './dependent/Charts/LineChart';
+// export { default as Stacked } from './dependent/Charts/Stacked';
+// export { default as Pie } from './dependent/Charts/Pie';
+// export { default as ChartsHeader } from './dependent/ChartsHeader';
+// export { default as Header } from './dependent/Header';
+
+// index.jsx (pages)
+// export { default as Ecommerce } from './Ecommerce';
+// export { default as Kanban } from './Kanban';
+// export { default as Orders } from './Orders';
+// export { default as Employees } from './Employees';
+// export { default as Editor } from './Editor';
+// export { default as Customers } from './Customers';
+// export { default as ColorPicker } from './ColorPicker';
+// export { default as Calendar } from './Calendar';
+// export { default as Area } from './Charts/Area';
+// export { default as Bar } from './Charts/Bar';
+// export { default as ColorMapping } from './Charts/ColorMapping';
+// export { default as Financial } from './Charts/Financial';
+// export { default as Line } from './Charts/Line';
+// export { default as Pie } from './Charts/Pie';
+// export { default as Pyramid } from './Charts/Pyramid';
+// export { default as Stacked } from './Charts/Stacked';
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,80 @@
+import type { AppProps } from 'next/app';
+import { FiSettings } from 'react-icons/fi';
 // import '../styles/globals.css';
 import '../styles/index.css';
-import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import TooltipComponent from '../components/independent/TooltipComponent';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	const [isMenuActive, setIsMenuActive] = useState(false);
+	const [currentMode, setCurrentMode] = useState<'dark' | 'light'>('light');
+	const [currentColor, setCurrentColor] = useState('');
+	const [themeSettings, setThemeSettings] = useState(false);
+
+	useEffect(() => {
+		document.getElementById('js-licensing')?.remove();
+		const currentThemeColor = localStorage.getItem('colorMode');
+		const currentThemeMode = localStorage.getItem('themeMode');
+		if (
+			currentThemeColor &&
+			currentThemeMode &&
+			// ['light', 'dark'].indexOf(currentThemeMode) !== -1
+			(currentThemeMode == 'light' || currentThemeMode == 'dark')
+		) {
+			setCurrentColor(currentThemeColor);
+			setCurrentMode(currentThemeMode);
+		}
+	}, []);
+
+	return (
+		<div
+			className={`${
+				currentMode === 'dark' ? 'dark' : ''
+			} flex relative dark:bg-main-dark-bg`}
+		>
+			<div className='fixed ring-4 bottom-4' style={{ zIndex: '1000' }}>
+				<TooltipComponent
+					content='settings'
+					position='topRight'
+					windowCollision
+				>
+					<button
+						onClick={() => setThemeSettings(true)}
+						className='p-3 text-3xl hover:drop-shadow-xl hover:bg-light-gray text-white'
+						style={{
+							background: 'blue',
+							borderRadius: '50%',
+						}}
+					>
+						<FiSettings />
+					</button>
+				</TooltipComponent>
+			</div>
+			{isMenuActive ? (
+				<div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+					{/* <Sidebar /> */}
+				</div>
+			) : (
+				<div className='w-0 dark:bg-secondary-dark-bg'>{/* <Sidebar /> */}</div>
+			)}
+			<div
+				className={
+					isMenuActive
+						? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+						: 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+				}
+			>
+				<div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
+					{/* <Navbar /> */}
+				</div>
+				<div>
+					{/* {themeSettings && (<ThemeSettings />)} */}
+					<Component {...pageProps} />
+				</div>
+				{/* <Footer /> */}
+			</div>
+		</div>
+	);
 }
 
 export default MyApp;
